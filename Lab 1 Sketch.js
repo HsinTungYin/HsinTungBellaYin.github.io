ace80ea9221f15b1b371db5f7106fe369792be2e
HsinTungYin <hsinyin@ocadu.ca>
2025-01-31 15:16:47 -0500

    Lab 1

--- Lab 1 Sketch.js
+++ Lab 1 Sketch.js
@@ -3,18 +3,18 @@ function setup(){
 }
 
 function draw(){
-    background(0, 150, 50);
-    fill ('aquamarine');
+    background("#65B5F6");
+    fill ('pink');
     stroke('white');
     for (var i = 0; i < 10000; i++){
-        rect((i*10)%width, (i*10)%height, 10, 10);
+        circle((i*10)%width, (i*10)%height, 10, 10);
     }
 
-    fill("blue");
-    stroke ('black');
+    fill("#8968CD");
+    stroke ('white');
     if (mouseX < 200){
         rect(mouseX, mouseY, 100, 100);
     }else{
-        rect(mouseX,mouseY, 50,50,25);
+        circle(mouseX,mouseY, 100,50,25);
     }
 }
